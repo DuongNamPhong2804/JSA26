d3d04d8426524342f0afedb28a2c430521222ebb
PatElvisEmulator <namphong28042010@gmail.com>
2023-08-19 19:30:22 +0700

    Update

---
+++ Day 6/script.js
@@ -0,0 +1,8 @@
+// Câu 1
+// Cách 1
+let clickMe = document.getElementById("myButton")
+console.log(clickMe);
+clickMe.addEventListener("click", function () {
+  console.log("hello my friend");
+});
+// Cách 2
